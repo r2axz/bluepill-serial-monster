3d068e907e4459036f97eb295c3c1196c0a9535c
Kirill Kotyagin <kirill.kotyagin@gmail.com>
2020-11-13 13:32:52 +0300

    ADD: configuration shell banner

--- cdc_shell.c
+++ cdc_shell.c
@@ -4,13 +4,23 @@
  * Copyright (c) 2020 Kirill Kotyagin
  */
 
+#include <string.h>
 #include "cdc_shell.h"
 
 static char cmd_line[USB_SHELL_MAX_CMD_LINE_SIZE];
 static char *cmd_line_p = cmd_line;
 
+static const char *cdc_shell_banner =   "\r\n\r\n"
+                                        "*******************************\r\n"
+                                        "* Configuration Shell Started *\r\n"
+                                        "*******************************\t\n"
+                                        "\r\n";
+static const char *cdc_shell_prompt = ">";
+
 void cdc_shell_init() {
     cmd_line_p = cmd_line;
+    _cdc_shell_write(cdc_shell_banner, strlen(cdc_shell_banner));
+    _cdc_shell_write(cdc_shell_prompt, strlen(cdc_shell_prompt));
 }
 
 void cdc_shell_exit() {
