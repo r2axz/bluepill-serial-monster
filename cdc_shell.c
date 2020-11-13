5002dec7cc5660502643ec1229bc3296c91d87da
Kirill Kotyagin <kirill.kotyagin@gmail.com>
2020-11-13 09:46:24 +0300

    ADD: command line buf

--- cdc_shell.c
+++ cdc_shell.c
@@ -1,7 +1,10 @@
 #include "cdc_shell.h"
 
-void cdc_shell_init() {
+static char cmd_line[USB_SHELL_MAX_CMD_LINE_SIZE];
+static char *cmd_line_p = cmd_line;
 
+void cdc_shell_init() {
+    cmd_line_p = cmd_line;
 }
 
 void cdc_shell_exit() {
--- cdc_shell.h
+++ cdc_shell.h
@@ -1,6 +1,8 @@
 #ifndef CDC_SHELL_H
 #define CDC_SHELL_H
 
+#define USB_SHELL_MAX_CMD_LINE_SIZE     0x100
+
 void cdc_shell_init();
 void cdc_shell_exit();
 
